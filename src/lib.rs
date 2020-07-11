3497dadf31b0cbcbe47e8878b01d51faa22d0a20
A Mackay <amk@gegroup.com.au>
2020-07-11 12:19:36 +1000

    Make throw expr work

--- src/lib.rs
+++ src/lib.rs
@@ -11,13 +11,19 @@ use nom::{
 #[derive(Debug, Eq, PartialEq)]
 enum Expr {
     Return { expr: Box<Expr> },
-    Throw,
+    Throw { expr: Box<Expr> },
     Literal(Literal),
 }
 
 impl Expr {
     fn new_return(expr: Expr) -> Self {
         Self::Return {
+            expr: Box::new(expr),
+        }
+    }
+
+    fn new_throw(expr: Expr) -> Self {
+        Self::Throw {
             expr: Box::new(expr),
         }
     }
@@ -38,6 +44,21 @@ impl Literal {
     }
 }
 
+fn parse_throw_expr(input: &str) -> IResult<&str, Expr> {
+    let (input, _) = parse_throw_keyword(input)?;
+    let (input, _) = parse_whitespace(input)?;
+    let (input, expr) = parse_expr(input)?;
+    let throw_expr = Expr::new_throw(expr);
+    Ok((input, throw_expr))
+}
+
+fn parse_throw_keyword(input: &str) -> IResult<&str, ()> {
+    map(
+        tag("throw"),
+        |_| ()
+    )(input)
+}
+
 fn parse_literal_expr(input: &str) -> IResult<&str, Expr> {
     let (input, literal_contents) = parse_double_quoted_string_literal(input)?;
     let expr = Expr::new_literal(Literal::str(literal_contents));
@@ -45,7 +66,7 @@ fn parse_literal_expr(input: &str) -> IResult<&str, Expr> {
 }
 
 fn parse_expr(input: &str) -> IResult<&str, Expr> {
-    alt((parse_literal_expr, parse_return_expr))(input)
+    alt((parse_literal_expr, parse_return_expr, parse_throw_expr))(input)
 }
 
 fn parse_whitespace(input: &str) -> IResult<&str, ()> {
@@ -96,6 +117,16 @@ mod tests {
     fn parse_expr_return_works() {
         let e = "return \"Hello\"";
         let expect = Expr::new_return(Expr::new_literal(Literal::str("Hello")));
+        assert_eq!(
+            parse_expr(e).unwrap(),
+            ("", expect)
+        );
+    }
+
+    #[test]
+    fn parse_expr_throw_works() {
+        let e = "throw \"Some error\"";
+        let expect = Expr::new_throw(Expr::new_literal(Literal::str("Some error")));
         assert_eq!(
             parse_expr(e).unwrap(),
             ("", expect)
