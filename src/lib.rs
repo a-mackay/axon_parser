06f35362a5710d4ca0c7b6ba3f2d0f20d829051a
A Mackay <opensauceburner@outlook.com>
2020-12-23 17:22:34 +1100

    Add top-level parsing functions, fix bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,71 +1,36 @@
 pub mod ast;
+pub use ast::Indent;
 
-// Dict(
-//     {
-//         TagName(
-//             "params",
-//         ): List(
-//             [],
-//         ),
-//         TagName(
-//             "body",
-//         ): Dict(
-//             {
-//                 TagName(
-//                     "type",
-//                 ): Lit(
-//                     Str(
-//                         "block",
-//                     ),
-//                 ),
-//                 TagName(
-//                     "exprs",
-//                 ): List(
-//                     [
-//                         Dict(
-//                             {
-//                                 TagName(
-//                                     "val",
-//                                 ): Lit(
-//                                     Str(
-//                                         "hello world",
-//                                     ),
-//                                 ),
-//                                 TagName(
-//                                     "type",
-//                                 ): Lit(
-//                                     Str(
-//                                         "literal",
-//                                     ),
-//                                 ),
-//                             },
-//                         ),
-//                     ],
-//                 ),
-//             },
-//         ),
-//         TagName(
-//             "type",
-//         ): Lit(
-//             Str(
-//                 "func",
-//             ),
-//         ),
-//     },
-// )
+use axon_parseast_parser::parse as ap_parse;
+use std::convert::TryInto;
+use thiserror::Error;
 
-// fn convert(val: Val) -> () {}
+/// If the axon input represents a function, return a `ast::Func`.
+pub fn parse_func(axon: &str) -> Result<ast::Func, Error> {
+    let val = &ap_parse(axon).map_err(|_| Error::Parse)?;
+    let func: Result<ast::Func, ()> = val.try_into();
+    let func = func.map_err(|_| Error::AstConversion)?;
+    Ok(func)
+}
 
-#[cfg(test)]
-mod tests {
-    use axon_parseast_parser::parse;
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Could not parse the representation of Axon code")]
+    Parse,
+    #[error(
+        "Could not parse the loosely-typed Axon Val into a strongly-typed AST"
+    )]
+    AstConversion,
+}
 
-    const HELLO_WORLD: &str = r###"{type:"func", params:[], body:{type:"block", exprs:[{type:"literal", val:"hello world"}]}}"###;
-
-    #[test]
-    fn it_works() {
-        let x = parse(HELLO_WORLD).unwrap();
-        println!("{:#?}", x);
-        assert_eq!(2 + 2, 4);
-    }
+/// If the axon input represents a function, parse it and return a `Vec<String>`,
+/// each string being a formatted line of code.
+pub fn parse_func_to_formatted_lines(
+    axon: &str,
+    indent: &Indent,
+) -> Result<Vec<String>, Error> {
+    let func = parse_func(axon)?;
+    let lines = func.to_lines(indent);
+    let strings = lines.into_iter().map(|line| format!("{}", line)).collect();
+    Ok(strings)
 }
